ccce464594fa623a0ecd649ea86c60e461bce6aa
TORS-LAPTOP\tgikl <t.gikling@hotmail.no>
2025-03-31 13:01:34 +0200

    Init commit and project creation for master thesis

---
+++ back-end/cybersecurity-awareness/src/main/java/ntnu/master/assessment/cybersecurityawareness/CybersecurityAwarenessApplication.java
@@ -0,0 +1,13 @@
+package ntnu.master.assessment.cybersecurityawareness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybersecurityAwarenessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CybersecurityAwarenessApplication.class, args);
+    }
+
+}
---
+++ back-end/cybersecurity-awareness/src/test/java/ntnu/master/assessment/cybersecurityawareness/CybersecurityAwarenessApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.master.assessment.cybersecurityawareness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybersecurityAwarenessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
