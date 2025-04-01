33404100eab79875560d1cd6f1155d856a019183
TORS-LAPTOP\tgikl <t.gikling@hotmail.no>
2025-04-01 14:44:20 +0200

    Updated header to include onclick to take the user back to main page

--- front-end/src/components/header/header.tsx
+++ front-end/src/components/header/header.tsx
@@ -1,12 +1,19 @@
 import "./header.css"
 import homeIcon from "../../assets/home.svg";
+import {useNavigate} from "react-router-dom";
 
 function Header() {
 
+    const navigate = useNavigate();
+
+    const homeClick = () => {
+        navigate("/");
+    }
+
     return (
       <div className="header-content">
-          <img className="header-img" src={(homeIcon)} alt="Home" />
-          <p className='username-txt'>Username</p>
+          <img className="header-img" src={(homeIcon)} alt="Home" onClick={homeClick} />
+          <a className='username-txt' onClick={homeClick}>Username</a>
       </div>
     );
 }
