409eae34f4c372099ca66b1dc223790a5048382a
TORS-LAPTOP\tgikl <t.gikling@hotmail.no>
2025-04-05 14:42:44 +0200

    Added connection to the database, and added dependency for .env file for the spring applicaiton

--- back-end/cybersecurity-awareness/src/main/java/ntnu/master/assessment/cybersecurityawareness/controller/UserController.java
+++ back-end/cybersecurity-awareness/src/main/java/ntnu/master/assessment/cybersecurityawareness/controller/UserController.java
@@ -1,4 +1,13 @@
 package ntnu.master.assessment.cybersecurityawareness.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @GetMapping("/user")
+    public String getAllUsers() {
+        return "Hello World";
+    }
 }
--- back-end/cybersecurity-awareness/src/main/java/ntnu/master/assessment/cybersecurityawareness/repository/UserRepository.java
+++ back-end/cybersecurity-awareness/src/main/java/ntnu/master/assessment/cybersecurityawareness/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package ntnu.master.assessment.cybersecurityawareness.repository;
 
-public interface UserRepository {
+import ntnu.master.assessment.cybersecurityawareness.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+
 }
