b436b25ecce19b336844b46b0ae99fbfe0d4c6bc
TORS-LAPTOP\tgikl <t.gikling@hotmail.no>
2025-04-21 13:04:53 +0200

    Working making JWT tokens last with a page refresh

---
+++ back-end/cybersecurity-awareness/src/main/java/ntnu/master/assessment/cybersecurityawareness/persistance/entity/CustomUserDetails.java
@@ -0,0 +1,54 @@
+package ntnu.master.assessment.cybersecurityawareness.persistance.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Setter
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final String username;
+    private final String password;
+    private final int orgId;
+    private final String role;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(String username,String password , int orgId, String role, Collection<? extends GrantedAuthority> authorities) {
+        this.username = username;
+        this.password = password;
+        this.orgId = orgId;
+        this.role = role;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
--- back-end/cybersecurity-awareness/src/main/java/ntnu/master/assessment/cybersecurityawareness/service/CustomUserDetailsService.java
+++ back-end/cybersecurity-awareness/src/main/java/ntnu/master/assessment/cybersecurityawareness/service/CustomUserDetailsService.java
@@ -1,34 +1,45 @@
 package ntnu.master.assessment.cybersecurityawareness.service;
 
+import ntnu.master.assessment.cybersecurityawareness.persistance.entity.CustomUserDetails;
 import ntnu.master.assessment.cybersecurityawareness.persistance.entity.User;
 import ntnu.master.assessment.cybersecurityawareness.persistance.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+
+import java.util.List;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+
     @Autowired
     public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
+
     }
 
+
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-       User user = userRepository.getUserByEmail(username);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+       User user = userRepository.getUserByEmail(email);
         if (user == null) {
-            throw new UsernameNotFoundException("User not found with email: " + username);
+            throw new UsernameNotFoundException("User not found with email: " + email);
         }
-       return org.springframework.security.core.userdetails.User
-               .withUsername(user.getEmail())
-               .password(user.getPassword())
-               .authorities(user.getRole())
-               .build();
+       return new CustomUserDetails(
+               user.getEmail(),
+               user.getPassword(),
+               user.getOrganizationId(),
+               user.getRole(),
+               List.of(() -> user.getRole())
+       );
     }
 
 }
